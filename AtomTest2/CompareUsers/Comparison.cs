30090d6b3b0ab71dd16e9831cbd882ace83b9e99
Kibatun <ifkfvjif@mail.ru>
2023-11-07 16:38:38 +0500

    Comparison.refactor: methods was created

--- AtomTest2/CompareUsers/Comparison.cs
+++ AtomTest2/CompareUsers/Comparison.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace AtomTest.UserComparison
 {
@@ -10,78 +6,10 @@ namespace AtomTest.UserComparison
     {
         public static void Compare(string[] args)
         {
-            Console.WriteLine("Введите данные первого пользователя (ФИО через пробел):");
+            User userOne = ReadUserInput("первого пользователя");
+            User userTwo = ReadUserInput("второго пользователя");
 
-            User user1 = new User();
-            string[] fullName1 = Console.ReadLine().Split(' ');
-            if (fullName1.Length != 3)
-            {
-                Console.WriteLine("Неправильный формат ввода ФИО.");
-                return;
-            }
-
-            user1.SurName = fullName1[0];
-            user1.FirstName = fullName1[1];
-            user1.Patronymic = fullName1[2];
-
-            Console.Write("Введите серию и номер паспорта (10 цифр): ");
-
-            string passportNumber1 = Console.ReadLine();
-            if (passportNumber1.Length != 10 || !int.TryParse(passportNumber1, out _))
-            {
-                Console.WriteLine("Неверный формат номера паспорта.");
-                return;
-            }
-            user1.PassportNumber = passportNumber1;
-
-            Console.Write("Введите дату рождения (ДД ММ ГГГГ через пробел): ");
-            string[] birthDateParts1 = Console.ReadLine().Split(' ');
-            if (birthDateParts1.Length != 3 ||
-                !int.TryParse(birthDateParts1[0], out int day) ||
-                !int.TryParse(birthDateParts1[1], out int month) ||
-                !int.TryParse(birthDateParts1[2], out int year))
-            {
-                Console.WriteLine("Неверный формат даты рождения.");
-                return;
-            }
-            user1.DateOfBirth = new DateTime(year, month, day);
-
-            Console.WriteLine("Введите данные второго пользователя (ФИО через пробел):");
-
-            User user2 = new User();
-            string[] fullName2 = Console.ReadLine().Split(' ');
-            if (fullName2.Length != 3)
-            {
-                Console.WriteLine("Неправильный формат ввода ФИО.");
-                return;
-            }
-
-            user2.SurName = fullName2[0];
-            user2.FirstName = fullName2[1];
-            user2.Patronymic = fullName2[2];
-
-            Console.Write("Введите серию и номер паспорта (10 цифр): ");
-            string passportNumber2 = Console.ReadLine();
-            if (passportNumber2.Length != 10 || !int.TryParse(passportNumber2, out _))
-            {
-                Console.WriteLine("Неверный формат номера паспорта.");
-                return;
-            } 
-            user2.PassportNumber = passportNumber2;
-
-            Console.Write("Введите дату рождения (ДД ММ ГГГГ через пробел): ");
-            string[] birthDateParts2 = Console.ReadLine().Split(' ');
-            if (birthDateParts2.Length != 3 ||
-                !int.TryParse(birthDateParts2[0], out day) ||
-                !int.TryParse(birthDateParts2[1], out month) ||
-                !int.TryParse(birthDateParts2[2], out year))
-            {
-                Console.WriteLine("Неверный формат даты рождения.");
-                return;
-            }
-            user2.DateOfBirth = new DateTime(year, month, day);
-
-            if (user1.IsIdentical(user2))
+            if (userOne.IsIdentical(userTwo))
             {
                 Console.WriteLine("Пользователи идентичны.");
             }
@@ -89,6 +17,81 @@ namespace AtomTest.UserComparison
             {
                 Console.WriteLine("Пользователи не идентичны.");
             }
+        }
+
+        private static User ReadUserInput(string userDescription)
+        {
+            Console.WriteLine($"Введите данные {userDescription} (ФИО через пробел):");
+            User user = new User();
+
+            string fullName = ReadLineWithValidation("Неправильный формат ввода ФИО.", ValidateFullName);
+            string[] fullNameParts = fullName.Split(' ');
+
+            if (fullNameParts.Length != 3)
+            {
+                Console.WriteLine("Неправильный формат ввода ФИО.");
+                Environment.Exit(1);
+            }
+
+            user.SurName = fullNameParts[0];
+            user.FirstName = fullNameParts[1];
+            user.Patronymic = fullNameParts[2];
+
+            Console.Write("Введите серию и номер паспорта (10 цифр): ");
+            user.PassportNumber = ReadLineWithValidation("Неверный формат номера паспорта.", s => s.Length == 10 && int.TryParse(s, out _));
+
+            Console.Write("Введите дату рождения (ДД ММ ГГГГ через пробел): ");
+            string birthDate = ReadLineWithValidation("Неверный формат даты рождения.", ValidateDate);
+
+            string[] birthDateParts = birthDate.Split(' ');
+            int day = int.Parse(birthDateParts[0]);
+            int month = int.Parse(birthDateParts[1]);
+            int year = int.Parse(birthDateParts[2]);
+            user.DateOfBirth = new DateTime(year, month, day);
+
+            return user;
+        }
+
+        private static string ReadLineWithValidation(string errorMessage, Func<string, bool> validator)
+        {
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                if (!validator(input))
+                {
+                    Console.WriteLine(errorMessage);
+                }
+            } while (!validator(input));
+            return input;
+        }
+
+        private static bool ValidateFullName(string input)
+        {
+            return Regex.IsMatch(input, @"^\S+\s+\S+\s+\S+$");
+        }
+
+        private static bool ValidateDate(string input)
+        {
+            string[] parts = input.Split(' ');
+            if (parts.Length != 3)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(parts[0], out int day) ||
+                !int.TryParse(parts[1], out int month) ||
+                !int.TryParse(parts[2], out int year))
+            {
+                return false;
+            }
+
+            if (day < 1 || day > 31 || month < 1 || month > 12 || year < 1900 || year > DateTime.Now.Year)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
